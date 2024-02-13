3d2baf6c834825f04e3006460508f0f3ae3f5639
igorgaidolfi <gandalf.4@outlook.it>
2024-02-13 16:26:09 +0100

    added layout app

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Create un nuovo progetto Laravel. Create le pagine includendo header e footer tr
 Create poi una rotta per visualizzare la lista di tutti i fumetti recuperati da un file inserito nella cartella config e abbellite il tutto sfruttando Sass.
 Bonus:
 Create più pagine istituzionali nelle quali importare sia header che footer. Decidete voi cosa mostrare  al loro interno.
+
+2.
+proseguite con l'esercizio di ieri, stessa repo. Dovete replicare quanto visto a lezione, quindi fare in modo che la pagina con l'elenco dei fumetti e quella del dettaglio del fumetto estendano un layout. La pagina del dettaglio del fumetto la dovete creare, così come dovete creare la rotta nel file web.php
+Occhio al passaggio di parametri ed a come recuperare l'elemento dell'array. Poichè è sconsigliato usare il titolo, seguite i passi visti a lezione: aggiungete un id ad ogni fumetto, con un valore incrementale (mi raccomando: no valori ripetuti per l'id). Per recuperare il singolo fumetto usate uno dei due metodi visti (foreach classico oppure array_filter + array_values).
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,38 +1,20 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layout.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Dc Comics</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-</head>
-
-<body>
-    @include('partials.header')
-    <main>
-        <div class="container">
-            <div class="row">
-                @foreach ($comics as $comic)
+@section('content')
+    <div class="container">
+        <div class="row">
+            @foreach ($comics as $comic)
                 <div class="col-2 p-2">
                     <div class="card">
-                        <img src="{{$comic['thumb']}}" class="card-img-top" alt="{{$comic['title']}}">
-                        <div class="card-body">
-                          <p class="card-title">{{$comic['title']}}</p>
-                        </div>
-                      </div>
+                        <a href="{{route('detail-comic', ['id' => $comic['id']])}}" class="link-dark link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
+                            <img src="{{$comic['thumb']}}" class="card-img-top" alt="{{$comic['title']}}">
+                            <div class="card-body">
+                                <p class="card-title ">{{$comic['title']}}</p>
+                            </div>
+                        </a>
                     </div>
-                    @endforeach
-            </div>
+                </div>
+            @endforeach
         </div>
-    </main>
-    @include('partials.footer')
-</body>
-
-</html>
+    </div>
+@endsection
